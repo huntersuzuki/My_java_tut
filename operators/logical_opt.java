9bf2cf68ce86551c199258b8073535fbff52a37f
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-03-19 20:32:18 +0530

    19-03-24 8:32 PM

---
+++ operators/logical_opt.java
@@ -0,0 +1,12 @@
+class Logical{
+    public static void main(String args[]){
+
+        int x=10,y=20,z=30;
+        boolean r;
+
+        //r= x>y && x<y; => False
+        //r = x>y || x<y; => True
+        //r =!(x>y || x<y); => False
+        System.out.println("The result is : "+r);
+    }
+}
