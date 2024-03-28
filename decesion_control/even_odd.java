6dae0f0dc853a3ca620e3fbd32fa230a3a9dd311
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-03-28 20:04:53 +0530

    Even odd program

--- decesion_control/even_odd.java
+++ decesion_control/even_odd.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class EvenOdd{
     public static void main(String args[]){
-        int userInput,process,output;
+        int userInput;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number : ");
         userInput = sc.nextInt();
-        process = userInput%2;
+        //process = userInput%2;
 
-        if(process != 0){
+        if(userInput %2 != 0){
             System.out.println(userInput+" is odd");
         }
         else{
