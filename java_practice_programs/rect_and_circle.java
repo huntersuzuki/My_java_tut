c0844424fe1466bbae96069008bceaa95690b5b6
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-04 16:58:41 +0530

    java practice programs

--- java_practice_programs/rect_and_circle.java
+++ java_practice_programs/rect_and_circle.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 class RectAndCircle {
-    public static void main(String args[]){
-        double lengthRect , breadthRect ,radiusCircle ,areaCircle ,cirCumCircle ,areaRect, perimeterRect;
+    public static void main(String args[]) {
+        double lengthRect, breadthRect, radiusCircle, areaCircle, cirCumCircle, areaRect, perimeterRect;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter length Of rectangle ");
         lengthRect = sc.nextDouble();
@@ -10,14 +10,14 @@ class RectAndCircle {
         breadthRect = sc.nextDouble();
         System.out.println("Enter radius of a circle");
         radiusCircle = sc.nextDouble();
-        areaRect = lengthRect*breadthRect;
-        perimeterRect = 2*(lengthRect+breadthRect);
-        areaCircle = 3.14*radiusCircle*radiusCircle;
-        cirCumCircle = 2*3.14*radiusCircle;
-        System.out.println(areaRect+" is area of rectangle");
-        System.out.println(perimeterRect+" is perimeter of rectangle");
+        areaRect = lengthRect * breadthRect;
+        perimeterRect = 2 * (lengthRect + breadthRect);
+        areaCircle = 3.14 * radiusCircle * radiusCircle;
+        cirCumCircle = 2 * 3.14 * radiusCircle;
+        System.out.println(areaRect + " is area of rectangle");
+        System.out.println(perimeterRect + " is perimeter of rectangle");
         System.out.println(areaCircle + " is area of circle");
-        System.out.println(cirCumCircle+" is the circumference of circle");
+        System.out.println(cirCumCircle + " is the circumference of circle");
 
     }
 }
--- java_practice_programs/stu_sub_marks.java
+++ java_practice_programs/stu_sub_marks.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 class StudentMarks {
-    public static void main(String args[]){
-        double sub1,sub2,sub3,sub4,sub5,aggrMarks,perMarks;
+    public static void main(String args[]) {
+        double sub1, sub2, sub3, sub4, sub5, aggrMarks, perMarks;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first subject marks :");
         sub1 = sc.nextDouble();
@@ -14,9 +14,10 @@ class StudentMarks {
         sub4 = sc.nextDouble();
         System.out.println("Enter fifth subject marks :");
         sub5 = sc.nextDouble();
-        aggrMarks = sub1+sub2+sub3+sub4+sub5;
-        System.out.println("Aggregate marks for "+sub1+" , "+sub2+" , "+sub3+" , "+sub4+" & "+sub5+" is "+aggrMarks);
-        perMarks = aggrMarks/5;
-        System.out.println("The percentage for the obtained marks is : "+perMarks);
+        aggrMarks = sub1 + sub2 + sub3 + sub4 + sub5;
+        System.out.println("Aggregate marks for " + sub1 + " , " + sub2 + " , " + sub3 + " , " + sub4 + " & " + sub5
+                + " is " + aggrMarks);
+        perMarks = aggrMarks / 5;
+        System.out.println("The percentage for the obtained marks is : " + perMarks);
     }
 }
