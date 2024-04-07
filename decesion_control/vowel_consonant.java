6df506a0473c098a98f652a52987f8d835d3b0de
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-07 15:59:19 +0530

    vowel consonant program

--- decesion_control/vowel_consonant.java
+++ decesion_control/vowel_consonant.java
@@ -6,7 +6,7 @@ class VowelConsonant {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a character");
         str = sc.next();
-        ch = str.charAt(0);
+        ch = str.charAt(0); // it returns the characters position.
         
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
             System.out.println("It's a vowel");
