597eedcd6606a620bb3f54556143d13ee56ce54d
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-16 16:02:43 +0530

    factorial using decrement operator

--- loop_control/factorial_decrement.java
+++ loop_control/factorial_decrement.java
@@ -1,4 +1,16 @@
 import java.util.*;
-class factorial_decrement {
-    
+class FactorialDecrement {
+    public static void main(String[] args) {
+        int fact = 1, i, n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to find it's factorial");
+        n = sc.nextInt();
+        i=n;
+        while (i > 0) {
+            fact = fact * i;
+            i--;
+            System.out.println("i= " + i + " fact= " + fact);
+        }
+        System.out.println(n + "!= " + fact);
+    }
 }
