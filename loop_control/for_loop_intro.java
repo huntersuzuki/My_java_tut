0b45e820347c73f2cf5e81d467b1e75d542b83fb
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-16 20:21:41 +0530

    for loop introduction

---
+++ loop_control/for_loop_intro.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ForLoopIntro {
+    public static void main(String args[]) {
+        int n,i;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value");
+        n = sc.nextInt();
+        for(i=1;i<=n;i++){
+            System.out.println("Hello World");
+        }
+    }
+}
