3ab95c4fa9f47cf527bd81171cc53c5d5bc19342
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-16 16:03:29 +0530

    Summation of even number

--- loop_control/summation_of_evennum.java
+++ loop_control/summation_of_evennum.java
@@ -1,4 +1,16 @@
 import java.util.*;
-class summation_of_evennum {
-    
+class SummationEvenNum {
+    public static void main(String[] args) {
+        int sum = 0, i, n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number for odd Summation");
+        n = sc.nextInt();
+        i = 2;
+        while (i <= n) {
+            sum = sum + i;
+            System.out.println("i= " + i + " sum= " + sum);
+            i = i + 2;
+        }
+        System.out.println(sum);
+    }
 }
