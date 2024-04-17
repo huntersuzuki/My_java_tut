a3c5efaa0b398f983e1ede4efedef968d8b81cc9
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-17 17:38:57 +0530

    Armstrong number program

--- decesion_control/armstrong_three_digit.java
+++ decesion_control/armstrong_three_digit.java
@@ -6,11 +6,11 @@ class ArmstrongThreeDigit {
         System.out.println("Enter a Three digit number: ");
         n = sc.nextInt();
         if (n>=100 && n<=999) {
-            a = n % 10;
-            b = n / 10;
-            c = b % 10;
-            d = b / 10;
-            sum = a * a * a + c * c * c + d * d * d;
+            a = n % 10; // 153%10=3 => 3 is seperated
+            b = n / 10; // 153/10=15 => 3 is not in the list as it got seperated.
+            c = b % 10; // 15%10=5 => 5 got seperated
+            d = b / 10; // 15/10=1 => 1 got seperated
+            sum = a * a * a + c * c * c + d * d * d; 
             System.out.println("Summation of " + n + " is " + sum);
             if (n == sum) {
                 System.out.println(n + " is Armstrong number");
