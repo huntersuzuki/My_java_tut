e3086610b4340639311017f7eed8f42ce140ce57
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-25 20:09:16 +0530

    one d pattern

---
+++ pattern_programs/one_d_pattern.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+class OneDpattern {
+    public static void main(String args[]){
+        int i,n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+        for(i=1;i<=n;i++){
+            System.out.print("*");
+        }
+    }
+}
