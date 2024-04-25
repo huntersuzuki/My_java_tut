332a146e26525a61165e9fc18644666cea400528
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-25 20:21:46 +0530

    2 d pattern decrement

---
+++ pattern_programs/2_d_pattern_dec.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class TwoDpatternDecrement {
+    public static void main(String args[]){
+        int i,j,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+        for(i=n;i>0;i--){
+            for(j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
