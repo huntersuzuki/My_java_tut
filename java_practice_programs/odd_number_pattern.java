1c616c1982f73e1cd23200200ec2be3857df0254
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-04-30 17:34:51 +0530

    odd number pattern program

---
+++ java_practice_programs/odd_number_pattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class OddNumberPattern {
+    public static void main(String[] args) {
+        int i, j, n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to print a pattern");
+        n = sc.nextInt();
+        for (i = 1; i <= n; i += 2) {
+            for (j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
