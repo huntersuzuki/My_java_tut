ce6951b1bc2f83341b9e155be159b83e002ec847
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-05-15 19:20:53 +0530

    for each loop for array access

--- array/foreach_loop.java
+++ array/foreach_loop.java
@@ -1,7 +1,11 @@
 class ForEachLoop {
     public static void main(String[] args) {
+        // float x[] = { 10, 20, 30, 40, 50 };
+        // for(int i:x){ //cannot convert from float to int.
+        //     System.out.println("Element: "+i);
+        // }
         int x[] = { 10, 20, 30, 40, 50 };
-        for(int i:x){
+        for(float i:x){ //can convert from int to float.
             System.out.println("Element: "+i);
         }
     }
