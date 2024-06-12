31753a9cab00e73b4bb627002d707c4a3e428b3b
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-06-12 20:15:35 +0530

    equals() method used to check if two or more strings are equal in value with each other

---
+++ strings/string_equals.java
@@ -0,0 +1,8 @@
+class StringEquals {
+    public static void main(String[] args) {
+        String str="PranayBhoir",str2="PranayBhoir";
+        boolean result;
+        result=str.equals(str2);
+        System.out.println("The Result is: "+result);
+    }
+}
