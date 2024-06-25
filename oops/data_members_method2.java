402e6e0eec7c284e974c8a0a858452a38b9e3da2
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-06-25 10:24:58 +0530

    Program that demostrates data memebers in oops java

---
+++ oops/data_members_method2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class UniversityData {
+    String name;
+    int rollNo;
+    double percentage;
+
+    void getData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        this.name = sc.nextLine();
+        System.out.println("Enter Roll no: ");
+        this.rollNo = sc.nextInt();
+        System.out.println("Enter Percentage");
+        this.percentage = sc.nextDouble();
+    }
+
+    void displayData() {
+        System.out.println("Student Information");
+        System.out.println("Name: " + this.name);
+        System.out.println("Roll no: " + this.rollNo);
+        System.out.println("Percentage: " + this.percentage);
+    }
+}
+
+class MainClass{
+    
+    public static void main(String[] args) {
+        UniversityData ud = new UniversityData();
+        UniversityData ud1 = new UniversityData();
+        ud.getData();
+        ud1.getData();
+        ud.displayData();
+        ud1.displayData();
+    }
+}
