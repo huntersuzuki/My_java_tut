ec7c0cd84108e1beac7aa129e266b28fa8ca9999
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-06-25 10:23:58 +0530

    Program that demostrates how a constructor is initialzed and called

---
+++ oops/constructor.java
@@ -0,0 +1,9 @@
+class ConstructorJava {
+    ConstructorJava(){
+        System.out.println("Hello from constructor");
+    }
+    public static void main(String[] args) {
+        ConstructorJava cj = new ConstructorJava();
+        ConstructorJava cc = new ConstructorJava();
+    }
+}
