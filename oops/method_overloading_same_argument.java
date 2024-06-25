930620417154374bc6703d35b73a73f49e3c0ebb
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-06-25 10:28:59 +0530

    Method overloading with same argument not possible

---
+++ oops/method_overloading_same_argument.java
@@ -0,0 +1,14 @@
+class MethodOverloadingSameArgument {
+    
+    void addition(int x,int y){
+        System.out.println("The addition is: "+(x+y));
+    }
+    void addition(int a, int b){
+        System.out.println("The addition 2 is: "+(a+b));
+    }
+
+    public static void main(String[] args) {
+        MethodOverloadingSameArgument ma = new MethodOverloadingSameArgument();
+        ma.addition(10, 20);
+    }
+}
