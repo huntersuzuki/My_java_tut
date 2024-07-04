98d5235479ebba0c267adcf41fce773fcb60bca3
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-07-04 12:13:27 +0530

    Operator overloading in java

---
+++ polymorphism/operator_overloading.java
@@ -0,0 +1,12 @@
+class OperatorOverloading{
+    public static void main(String[] args) {
+        int x,y,z;
+        String a="pranay",b="Bhoir",c;
+        x=10;
+        y=20;
+        z=x+y;
+        System.out.println("The addition is: "+z);
+        c=a+b;
+        System.out.println("The concated string is: "+c);
+    }
+}
