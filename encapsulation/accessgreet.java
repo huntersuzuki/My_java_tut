749352a0740b8f9e78a58add3955bf70de470d2e
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-07-09 15:57:16 +0530

    introduction to packages and import of classes

---
+++ encapsulation/accessgreet.java
@@ -0,0 +1,11 @@
+import mypackage2.GreetAll;
+
+class AccessGreet extends GreetAll{
+    public static void main(String[] args) {
+        // GreetAll ga = new GreetAll();
+        // ga.goodAfterNoon();
+
+        AccessGreet ag = new AccessGreet();
+        ag.goodAfterNoon();
+    }
+}
---
+++ encapsulation/hello.java
@@ -0,0 +1,10 @@
+package mypackage1;
+
+class hello {
+    void greet(){
+        System.out.println("Hello from packaged class");
+    }   
+    public static void main(String[] args) {
+        System.out.println("Hii from main method");
+    }
+}
