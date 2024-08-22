2aa9c9d97447b9685c764f47f3a9e2439a4ef3da
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-08-22 13:23:52 +0530

    Finding gcd of two numbers using Euclidean Algorithm

---
+++ practice_programs/gcd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ class GCD {
+     public static void main(String[] args) {
+         int num1, num2, remainder, quotient;
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter 1st number");
+         num1 = sc.nextInt();
+         System.out.println("Enter 2nd number");
+         num2 = sc.nextInt();
+         remainder = num2 % num1;
+         quotient = num2 / num1;
+         for (int i = 1; i < quotient; i++) {
+             remainder = num1 % remainder;
+             if (num1 % remainder == 0) {
+                 break;
+             }
+         }
+         System.out.println("GCD = " + remainder);
+     }
+}
