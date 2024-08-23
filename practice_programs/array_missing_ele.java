5a8fc0437a89e42e605b076f8a95444ae597090c
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-08-23 20:11:36 +0530

    program to find the missing elements in array

--- practice_programs/array_missing_ele.java
+++ practice_programs/array_missing_ele.java
@@ -1,18 +1,31 @@
-class ArrayMissingEle {
-    public static void main(String[] args) {
+    import java.util.*;
 
-        int x[] = { 10, 20, 50, 30, 80, 90 };
-        int y[] = { 10, 60, 70, 40, 80, 90 };
-        int res = 0, count = 0;
-        for (int i = 0; i <= x.length - 1; i++) {
-            for (int j = 0; j <= y.length - 1; j++) {
-                if (x[i] != y[i]) {
-                    res = x[i];
-                    count++;
-                    System.out.println("missing element " + count + " = " + res);
-                    break;
+    class ArrayMissingEle {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int p;
+            System.out.println("Set the Array size: ");
+            p = sc.nextInt();
+            int x[] = new int[p];
+            int y[] = new int[p];
+            int res = 0, count = 0;
+            System.out.println("Enter first array elements: ");
+            for (int a = 0; a < x.length; a++) {
+                x[a] = sc.nextInt();
+            }
+            System.out.println("Enter second array elements: ");
+            for (int b = 0; b < y.length; b++) {
+                y[b] = sc.nextInt();
+            }
+            for (int i = 0; i <= x.length - 1; i++) {
+                for (int j = 0; j <= y.length - 1; j++) {
+                    if (x[i] != y[i]) {
+                        res = x[i];
+                        count++;
+                        System.out.println("missing element " + count + " = " + res);
+                        break;
+                    }
                 }
             }
         }
     }
-}
