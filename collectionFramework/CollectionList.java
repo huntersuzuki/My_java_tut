62c1031ab531d647d6f3d5b1da0442b977eba6b8
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-02 17:37:40 +0530

    Collection frameWork List

---
+++ collectionFramework/CollectionList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionList {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<Integer>();
+        nums.add(10);
+        nums.add(34);
+        nums.add(1);
+        nums.add(12);
+        nums.add(89);
+        nums.add(66);
+
+       System.out.println(nums.get(4)); // List also provides us with a 
+       // method that gives us the elemnents index.
+       for (Integer integer : nums) {
+        
+           System.out.println(integer);
+       }
+    }
+}
