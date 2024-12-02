5d221afd483136cafb472274cb14e77efdbe18ec
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-02 17:38:08 +0530

    Collection frameWork Set(TreeSet)

---
+++ collectionFramework/CollectionTreeSet.java
@@ -0,0 +1,17 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CollectionTreeSet {
+    public static void main(String[] args) {
+         Set<Integer> nums = new TreeSet<Integer>();
+        nums.add(10);
+        nums.add(34);
+        nums.add(1);
+        nums.add(12);
+        nums.add(12);
+        nums.add(66);
+
+        // TreeSet sorts our elements and also returns unique elements.
+        System.out.println(nums); // output -> [1, 10, 12, 34, 66]
+    }
+}
